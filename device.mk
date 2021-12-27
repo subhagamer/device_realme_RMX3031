cd212f2410eb7f1466dbf043ebe69ffa789b65c0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-12-27 12:19:19 +0530

    RMX3031: Disable ART debug and optimize dexpreopt
     * minimize java debug info
     * use everything filter for dexpreopt (https://github.com/AOSPA/android_vendor_pa/commit/ab5061c425e61bc7d36f7bcf480feea975795545)
     * disable debug configs in art
     * use non-debug dex2oat
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -48,6 +48,12 @@ AB_OTA_UPDATER := false
 PRODUCT_PACKAGES += \
     audio.a2dp.default
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
